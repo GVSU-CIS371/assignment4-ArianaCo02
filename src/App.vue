ec68b89f4e0a3b034a8de657c2ad17f6b3e07455
ArianaCo02 <144282253+ArianaCo02@users.noreply.github.com>
2025-11-21 14:47:11 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,79 +1,69 @@
 <template>
   <div>
-    <Beverage :isIced="beverageStore.currentTemp === 'Cold'" />
+    <!-- Beverage Mug Display -->
+    <Beverage
+      :isIced="beverageStore.selectedTemperature === 'Cold'"
+      :base="beverageStore.selectedBase.name"
+      :syrup="beverageStore.selectedSyrup.name"
+      :creamer="beverageStore.selectedCreamer.name"
+    />
+
+    <!-- Temperature Toggle -->
     <ul>
       <li>
-        <template v-for="temp in beverageStore.temps" :key="temp">
+        <template v-for="temp in beverageStore.temperatures" :key="temp">
           <label>
             <input
               type="radio"
               name="temperature"
               :id="`r${temp}`"
               :value="temp"
-              v-model="beverageStore.currentTemp"
+              v-model="beverageStore.selectedTemperature"
             />
             {{ temp }}
           </label>
         </template>
       </li>
     </ul>
-    <ul>
-      <li>
-        <template v-for="b in beverageStore.bases" :key="b.id">
-          <label>
-            <input
-              type="radio"
-              name="bases"
-              :id="`r${b.id}`"
-              :value="b"
-              v-model="beverageStore.currentBase"
-            />
-            {{ b.name }}
-          </label>
-        </template>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <template v-for="s in beverageStore.syrups" :key="s.id">
-          <label>
-            <input
-              type="radio"
-              name="syrups"
-              :id="`r${s.id}`"
-              :value="s"
-              v-model="beverageStore.currentSyrup"
-            />
-            {{ s.name }}
-          </label>
-        </template>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <template v-for="c in beverageStore.creamers" :key="c.id">
-          <label>
-            <input
-              type="radio"
-              name="creamers"
-              :id="`r${c.id}`"
-              :value="c"
-              v-model="beverageStore.currentCreamer"
-            />
-            {{ c.name }}
-          </label>
-        </template>
-      </li>
-    </ul>
-    <input type="text" placeholder="Beverage Name" />
-    <button>🍺 Make Beverage</button>
+
+    <!-- Name input -->
+    <input
+      type="text"
+      placeholder="Beverage Name"
+      v-model="beverageStore.beverageName"
+    />
+
+    <!-- Make Beverage button -->
+    <button @click="beverageStore.makeBeverage">🍺 Make Beverage</button>
+
+    <!-- Saved Beverage List -->
+    <div id="beverage-container" style="margin-top: 20px">
+      <h3>Saved Beverages:</h3>
+
+      <div v-if="beverageStore.beverages.length === 0">
+        <em>No beverages saved yet.</em>
+      </div>
+
+      <ul v-else>
+        <li
+          v-for="drink in beverageStore.beverages"
+          :key="drink.name"
+          style="margin: 4px 0;"
+        >
+          <button @click="beverageStore.showBeverage(drink)">
+            {{ drink.name }}
+          </button>
+        </li>
+      </ul>
+    </div>
   </div>
-  <div id="beverage-container" style="margin-top: 20px"></div>
 </template>
 
 <script setup lang="ts">
 import Beverage from "./components/Beverage.vue";
 import { useBeverageStore } from "./stores/beverageStore";
+
+// connect to the Pinia store
 const beverageStore = useBeverageStore();
 </script>
 
@@ -88,6 +78,7 @@ html {
   background-color: #6e4228;
   background: linear-gradient(to bottom, #6e4228 0%, #956f5a 100%);
 }
+
 ul {
   list-style: none;
 }
