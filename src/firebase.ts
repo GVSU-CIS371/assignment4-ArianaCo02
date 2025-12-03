35b01de56d48882408e222bbd688ea422c5464f5
ArianaCo02 <144282253+ArianaCo02@users.noreply.github.com>
2025-12-02 22:23:23 -0500

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -2,11 +2,10 @@ import { getFirestore } from "firebase/firestore";
 import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
-  const firebaseConfig = {
   apiKey: "AIzaSyDZpMZp6NiILlGEr77eTZLFAlYT_825ghc",
   authDomain: "cis371-947ba.firebaseapp.com",
   projectId: "cis371-947ba",
-  storageBucket: "cis371-947ba.firebasestorage.app",
+  storageBucket: "cis371-947ba.appspot.com",
   messagingSenderId: "919604137126",
   appId: "1:919604137126:web:2930ba67dc1bf06cd97867"
 };
